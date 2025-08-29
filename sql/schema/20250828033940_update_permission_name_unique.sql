710559360ccbde107481d707781f7f46ed7ab71e
sinh <sinh@gmail.com>
2025-08-29 09:52:53 +0700

    [AUTH] SCD-64: Update migrate permissions table

--- sql/schema/20250828033940_update_permission_name_unique.sql
+++ sql/schema/20250828033940_update_permission_name_unique.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
-Alter table permission
+Alter table permissions
     Drop constraint if exists permission_name_unique,
     Add constraint permission_name_unique unique (name);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-Alter table permission
+Alter table permissions
     Drop constraint if exists permission_name_unique;
 -- +goose StatementEnd
